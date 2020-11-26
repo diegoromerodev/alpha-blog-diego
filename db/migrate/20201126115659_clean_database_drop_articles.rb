48d581988d4323862f08118242154569dd00998e
Diego Romero <diegoromeropersonal@gmail.com>
2020-11-26 09:09:56 -0300

    New articles table

---
+++ db/migrate/20201126115659_clean_database_drop_articles.rb
@@ -0,0 +1,5 @@
+class CleanDatabaseDropArticles < ActiveRecord::Migration[6.0]
+  def change
+  	drop_table :articles
+  end
+end
---
+++ db/migrate/20201126120428_add_text_field.rb
@@ -0,0 +1,5 @@
+class AddTextField < ActiveRecord::Migration[6.0]
+  def change
+  	add_column :articles, :description, :text
+  end
+end
