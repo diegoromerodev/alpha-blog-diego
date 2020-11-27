4d743246de99c13d63ab9d044656a311d2aa7fc3
Diego Romero <diegoromeropersonal@gmail.com>
2020-11-27 12:41:04 -0300

    Edit post functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,36 @@ class ArticlesController < ApplicationController
   def show
   	@article = Article.find(params[:id])
   end
+
+  def index
+  	@articles = Article.all
+  end
+
+  def new
+  	@article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+  	@article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Post saved succesfully!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Post edited succesfully!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   get 'about', to: 'pages#about'
 end
